ef1db581d8aba3ef45db1776d4653c7cfb742344
Jhonney Eduardo Correa <Jhonneycp@gmail.com>
2020-09-30 01:02:43 -0300

    Modificación de enlace de 100tifi.co

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
         <div class="Header-logo">
         <h1>
-            <a href="/">
+            <a href="https://jhonneycp.github.io/Cientifico/">
                 100tifi.co
             </a>
         </h1>
